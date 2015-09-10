5f0e518640e20567fb56d43d1024d8b0705c77ba
JUN <15850761665@163.com>
2015-09-10 11:24:33 +0800

    add projectt

---
+++ blogserver-common/src/main/java/com/landaojia/blogserver/common/exception/CommonExceptionCode.java
@@ -0,0 +1,31 @@
+package com.landaojia.blogserver.common.exception;
+
+/**
+ * 异常代码枚举
+ * @author liuxi
+ */
+public enum CommonExceptionCode {
+
+    INCORRECT_PASSWORD("密码错误！"),
+    USER_NOT_EXISTS("该用户不存在！"),
+    USER_IS_EXISTS("用户名已被注册！"),
+    E999999("系统异常");
+
+
+    /**
+     * 消息
+     */
+    private String message;
+
+    private CommonExceptionCode( String message ){
+        this.message = message;
+    }
+
+    public String getCode(){
+        return this.name();
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
---
+++ blogserver-service/src/main/java/com/landaojia/blogserver/service/user/UserService.java
@@ -0,0 +1,15 @@
+package com.landaojia.blogserver.service.user;
+
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by JUN on 15/9/8.
+ */
+
+public interface UserService {
+
+     void registerUser(String uname,String pwd);
+
+
+}
