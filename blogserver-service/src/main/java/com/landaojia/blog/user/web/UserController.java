34f97a7364e109f610e7b61a5e493891259e747a
Winton Wu <winton.wu@lanyon.com>
2015-09-11 14:39:40 +0800

    add api and add config file

--- blogserver-service/src/main/java/com/landaojia/blog/user/web/UserController.java
+++ blogserver-service/src/main/java/com/landaojia/blog/user/web/UserController.java
@@ -1,21 +1,30 @@
 package com.landaojia.blog.user.web;
 
-import com.landaojia.blog.blog.common.result.JsonResult;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.landaojia.blog.blog.common.result.JsonResult;
 
 /**
  * Created by JUN on 15/9/11.
  */
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
     @ResponseBody
-    @RequestMapping("/login")
-    public JsonResult login(){
-        return JsonResult.success("登陆成功");
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public JsonResult login(@PathVariable("username") String username, @PathVariable("password") String password){
+        return JsonResult.success("ok");
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public JsonResult logout(){
+        return JsonResult.success("ok");
     }
 
 }
