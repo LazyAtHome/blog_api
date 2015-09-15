e99a9891630bd343a7a7ab8c1e2950bbe57ab555
jyyyxy <xy.jiang@landaojia.com>
2015-09-15 16:29:13 +0800

    delete some exceptioncode

--- blogserver-common/src/main/java/com/landaojia/blog/common/exception/CommonExceptionCode.java
+++ blogserver-common/src/main/java/com/landaojia/blog/common/exception/CommonExceptionCode.java
@@ -9,9 +9,7 @@ public enum CommonExceptionCode {
     INCORRECT_PASSWORD("密码错误！"),
     USER_NOT_EXISTS("该用户不存在！"),
     USER_IS_EXISTS("用户名已被注册！"),
-    USER_NOT_LOGIN("用户未登录！"),
-    POST_NOT_EXISTS("该博客不存在！"),
-    POST_NO_AUTH("没有操作权限"),
+    USER_IS_LOGINED("请勿重复登录！"),
     E999999("系统异常");
 
 
